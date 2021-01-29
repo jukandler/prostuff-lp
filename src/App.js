b8fb1d7ce56f3086542dfd45b41ce77c44dff186
Julia Kandler <julia.kandler@sz.de>
2021-01-29 09:07:51 +0100

    setup react project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hier entsteht die PROSTUFF Website.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
