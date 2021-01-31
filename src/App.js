43453d4c52846d6b755e6ce33afec8df5198795b
Julia Kandler <julia.kandler@sz.de>
2021-01-31 18:00:02 +0100

    aff Montserrat fonts

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ function App() {
                 <BenefitRow/>
             </div>
             <div className="benefit-container">
-                <Benefit headline="Entdecke die Ausstattung der Profis" text="Entdecke die besten Produkte für Sportler und Empfehlungen der Profis. Egal ob du dich für Fitness, Yoga oder Joggen interessierst, wir haben die besten Produktempfehlungen für dich, um dich deinen sportlichen Zielen näher zu bringen."/>
-                <Benefit headline="Hole dir Inspiration von Gleichgesinnten" text="Folge anderen Nutzern aus der Communit, lasse dich inspirieren und verpasse keine neuen Produktempfehlungen mehr. Speichere die besten Empfehlungen und kaufe sie direkt im Shop." reverse={true}/>
-                <Benefit headline="Teile deine Lieblingsprodukte" text="Teile selbst deine eigenen Lieblingsprodukte und Empfehlungen mit deiner Commnuity und hilf so anderen dabei sich zu verbessern und ihre Ziele zu erreichen."/>
+                <Benefit headline="Entdecke die Ausstattung der Profis" text="Finde heraus, welche Produkte die Profis nutzen. Egal ob du dich für Fitness, Yoga oder Joggen interessierst: Bei PROSTUFF findest du die beste Ausstattung, die dich deinen sportlichen Zielen näher bringt."/>
+                <Benefit headline="Hole dir Inspiration von Gleichgesinnten" text="Folge anderen Sportlern aus der Community, lasse dich inspirieren und bleibe auf dem Laufenden, welche neuen Produkte deine Bekannten empfehlen. Speichere die besten Empfehlungen auf deine Merkliste." reverse={true}/>
+                <Benefit headline="Teile deine Lieblingsprodukte" text="Teile selbst deine eigenen Lieblingsprodukte und Empfehlungen mit deiner Community. Zeige, auf welche Sportausstattung du setzt und hilf so anderen dabei, sich zu verbessern und ihre sportlichen Ziele zu erreichen."/>
             </div>
             <AppLink/>
             <Footer/>
