347825241a0bf4ab5d82bcdcf42a9e78e9a0a0aa
Julia Kandler <julia.kandler@sz.de>
2021-02-01 15:03:21 +0100

    adjust wording in header

--- src/Header.js
+++ src/Header.js
@@ -5,15 +5,15 @@ import Button from './Button';
 function Header() {
   return (
     <div className="header">
-      <img src={logo} alt="prostuff Logo" className="header-logo"/>
-      <h1 className="headline">
-          Prostuff App
-      </h1>
-      <h2 className="subheadline">
-          Entdecke die besten Produkte zu deinen Lieblingssportarten von ambitionierten Sportlern
-      </h2>
-      <Button/>
-      <button className="blocker">Bald verfügbar!</button>
+        <img src={logo} alt="prostuff Logo" className="header-logo"/>
+        <h1 className="headline">
+            Prostuff App
+        </h1>
+        <h2 className="subheadline">
+            Entdecke die besten Produkte ambitionierter Sportler zu deinen Lieblingssportarten
+        </h2>
+        <Button/>
+        <button className="blocker">Bald verfügbar!</button>
     </div>
   );
 }
