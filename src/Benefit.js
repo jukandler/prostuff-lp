e8dae5b992fcc90ccaed452df9e05ffa62f442c7
Julia Kandler <julia.kandler@sz.de>
2021-02-16 19:10:06 +0100

    exchange screenshots

--- src/Benefit.js
+++ src/Benefit.js
@@ -4,11 +4,11 @@ function Benefit(props) {
   const {reverse, headline, text, logo} = props;
   return (
     <div className={`benefit ${reverse ? "reverse" : ""}`}>
-        <img src={logo} className="benefit-image" alt="logo" />
         <div className="text-block">
             <h2 className="h2">{headline}</h2>
             <p>{text}</p>
         </div>
+        <img src={logo} className="benefit-image" alt="logo" />
     </div>
   );
 }
