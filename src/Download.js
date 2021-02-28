05341fc155fb21993c130f7006ea258e744d2bd3
Julia Kandler <julia.kandler@sz.de>
2021-02-28 15:42:05 +0100

    add redirect to appStore

--- src/Download.js
+++ src/Download.js
@@ -4,11 +4,30 @@ import './Download.css';
 
 function Download() {
 
+  function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+  }  
+
+  docReady(function() {
+    if(navigator.userAgent.toLowerCase().indexOf("android") > -1) {
+      window.location.href = 'http://play.google.com/store/apps/details?id=com.prostuff.prostuff';
+    }
+    if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1) {
+      window.location.href = 'https://apps.apple.com/de/app/prostuff/id1553954162?l=de';
+    }
+  }); 
+
   return (
     <div className="redirect">
-        <img src={logo} className="App-logo" alt="logo"/>
+        <img src={logo} className="logo" alt="logo"/>
         <p>
-          Download im AppStore. Hier kommt ein Redirect rein.
+          Du wirst in den App Store weitergeleitet...
         </p>
     </div>
   );
