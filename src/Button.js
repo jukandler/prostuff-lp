d519dfbfcdb72ea20196aded60d4803780449326
Timm Gaber <timm.gaber@web.de>
2021-03-02 21:27:08 +0100

    updated button for app store download

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,20 @@
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import './Button.css';
 
 function Button() {
   return (
-      <button className="button" disabled>
-        Bald im App Store verfügbar
-      </button>
+    <div>
+      <Link to="https://apps.apple.com/de/app/prostuff/id1553954162?l=de" target="_blank"> 
+        <button className="button" >
+          App-Download
+        </button>
+        </Link>
+        <Link to="http://play.google.com/store/apps/details?id=com.prostuff.prostuff" target="_blank"> 
+        <button className="button" >
+          App-Download
+        </button>
+        </Link>
+      </div>
   );
 }
 
